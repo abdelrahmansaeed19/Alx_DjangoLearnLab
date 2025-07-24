fcfa7f8a185b99f79708889a3e67b507b9cb4814
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2025-07-24 18:49:04 +0300

    Update Commit

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class RelationshipAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
+
+def ready(self):
+    import relationship_app.signals  # Import the signals module to ensure signal handlers are registered
+    # This will ensure that the signal handlers are connected when the app is ready
+    # and will not cause any issues with circular imports.
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),  # ← views.register (function-based)
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
     
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # ← required pattern
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # ← required pattern
