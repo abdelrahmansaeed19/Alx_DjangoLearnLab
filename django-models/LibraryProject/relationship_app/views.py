2274537a4a6c67076600e9485a9390fa482bff2a
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2025-07-24 18:06:59 +0300

    update commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
+# views.py
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
-# Create your views here.
+def book_list_view(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
