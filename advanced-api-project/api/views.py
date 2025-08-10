179310bf3505c318344d2322e9f40a3498ec33ce
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2025-08-10 20:33:19 +0300

    Update Commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 """
 Views Summary:
@@ -64,6 +66,15 @@ class UpdateView(generics.UpdateAPIView):
         """
         serializer.save()
 
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class DeleteView(generics.DestroyAPIView):
     """Delete a book. Only for authenticated users."""
