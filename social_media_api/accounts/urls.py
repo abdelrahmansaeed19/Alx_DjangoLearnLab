78644878afe0b70da7e8d6ad42ce511fc0d033d0
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2025-08-21 23:29:31 +0300

    Update

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, ProfileView, LogoutView, UserViewSet
+from .views import RegisterView, LoginView, ProfileView, LogoutView, UserViewSet, FollowUserView, UnfollowUserView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -11,5 +11,7 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("users/<int:pk>/followuser/", FollowUserView.as_view(), name="follow_user"),
+    path("users/<int:pk>/unfollowuser/", UnfollowUserView.as_view(), name="unfollow_user"),
     path("", include(router.urls)),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,6 +8,7 @@ from .models import CustomUser
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
 
 from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
 
@@ -57,17 +58,38 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    @action(detail=True, methods=["post"], permission_classes=[permissions.IsAuthenticated])
-    def follow(self, request, pk=None):
-        user_to_follow = self.get_object()
+    # @action(detail=True, methods=["post"], permission_classes=[permissions.IsAuthenticated])
+    # def follow(self, request, pk=None):
+    #     user_to_follow = self.get_object()
+    #     if request.user == user_to_follow:
+    #         return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+
+    #     request.user.following.add(user_to_follow)
+    #     return Response({"status": f"You are now following {user_to_follow.username}"})
+
+    # @action(detail=True, methods=["post"], permission_classes=[permissions.IsAuthenticated])
+    # def unfollow(self, request, pk=None):
+    #     user_to_unfollow = self.get_object()
+    #     request.user.following.remove(user_to_unfollow)
+    #     return Response({"status": f"You unfollowed {user_to_unfollow.username}"})
+
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        user_to_follow = get_object_or_404(CustomUser, pk=pk)
         if request.user == user_to_follow:
             return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.add(user_to_follow)
-        return Response({"status": f"You are now following {user_to_follow.username}"})
+        return Response({"status": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=["post"], permission_classes=[permissions.IsAuthenticated])
-    def unfollow(self, request, pk=None):
-        user_to_unfollow = self.get_object()
+
+# Unfollow User View
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        user_to_unfollow = get_object_or_404(CustomUser, pk=pk)
         request.user.following.remove(user_to_unfollow)
-        return Response({"status": f"You unfollowed {user_to_unfollow.username}"})
+        return Response({"status": f"You unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
