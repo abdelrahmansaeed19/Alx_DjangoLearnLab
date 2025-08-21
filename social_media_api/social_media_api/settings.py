02c5d51739a36be26767242d62a1d877fd19e2cc
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2025-08-22 00:58:17 +0300

    Update

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fv66_byu8x4%p!gm#ge$7$9a-5-b#3&sac+o9rc_!uzo+40x!3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
