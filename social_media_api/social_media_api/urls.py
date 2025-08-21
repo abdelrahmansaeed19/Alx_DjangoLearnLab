e043f6e3aafacd5b7d3b545ba1d646a05fac7042
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2025-08-21 22:54:51 +0300

    Update

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))  # Include the blog app's URLs
+    path("api/", include('posts.urls'))  # Include the blog app's URLs
 ]
